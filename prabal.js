9b55816dc8e001ffcd3f65a92a0dfdba84b64d8c
Prabal <prabalb122@gmail.com>
2023-02-23 10:20:39 +0530

    Made navbar responsive

---
+++ prabal.js
@@ -0,0 +1,10 @@
+burger = document.querySelector('.burger')
+nav = document.querySelector('#nav')
+logo = document.querySelector('.logo')
+navbar = document.querySelector('.navbar')
+
+burger.addEventListener('click',()=>{
+    nav.classList.toggle('opty');
+    logo.classList.toggle('opty');
+    navbar.classList.toggle('h-nav');
+})
